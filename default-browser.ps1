127c2a0e6c64c53f82133e0a70c3068cba5a67fb
Urs Roesch <github@bun.ch>
2021-10-13 12:35:28 +0200

    Add default-browser.ps1
    Summary:
      * A script to be run via windows autostart to change the
        default browser.

---
+++ default-browser.ps1
@@ -0,0 +1,89 @@
+# -----------------------------------------------------------------------------
+# Small script to set the default browser in HKCU to a browser of choice.
+#   This script was originally conceived in an virtual environment where each
+#   login spawned a new machine an the user settings were not kept across
+#   sessions. 
+# -----------------------------------------------------------------------------
+
+Param (
+  $BrowserName = "Firefox" 
+)
+
+# -----------------------------------------------------------------------------
+# Classes
+# -----------------------------------------------------------------------------
+class Browser {
+  [String] $AppPath
+  [String] $DefaultIcon  = "{0},1" -f $This.AppPath
+  [String] $ShellCommand = '"{0}" "%1"' -f $This.AppPath
+}
+  
+class Firefox : Browser {
+  [String] $AppPath      = "$env:ProgramFiles\Mozilla Firefox\firefox.exe" 
+  [String] $ShellCommand = '"{0}" -osint -url "%1"' -f $This.AppPath
+}
+
+class Chrome : Browser {
+  [String] $AppPath = "$env:ProgramFiles\Google\Chrome\Application\chrome.exe"
+}
+
+# -----------------------------------------------------------------------------
+# Functions
+# -----------------------------------------------------------------------------
+Function Create-RegistryTree() {
+  Param(
+    [String] $Protocol,
+    [Object] $WebBrowser
+  )
+  $RootKey = "HKCU:\Software\Classes\$Protocol"
+
+  # First recursivly remove the key
+  If (Test-Path $RootKey) {
+    Remove-Item -Path $RootKey -Recurse
+  }
+
+  # 
+  New-Item -Path "$RootKey"
+  New-ItemProperty -Path $RootKey `
+    -Name 'URL Protocol' -PropertyType String -Value ''
+  New-ItemProperty -Path $RootKey `
+    -Name 'EditFlags' -PropertyType Dword -Value '00000002'
+
+  New-Item -Path "$RootKey\DefaultIcon"
+  New-ItemProperty -Path "$RootKey\DefaultIcon" `
+    -Name '(Default)' -PropertyType String -Value $WebBrowser.DefaultIcon
+
+  New-Item -Path "$RootKey\shell"
+  New-ItemProperty -Path "$RootKey\shell" `
+    -Name '(Default)' -PropertyType String -Value 'open'
+
+  New-Item -Path "$RootKey\shell\open"
+  New-Item -Path "$RootKey\shell\open\command"
+  New-ItemProperty -Path "$RootKey\shell\open\command" `
+    -Name '(Default)' -PropertyType String -Value $WebBrowser.ShellCommand
+
+  New-Item -Path "$RootKey\shell\open\command\ddeexec"
+  New-ItemProperty -Path "$RootKey\shell\open\command\ddeexec" `
+    -Name '(Default)' -PropertyType String -Value ''
+}
+
+Function DefaultBrowser() {
+  Param(
+    [String] $Name
+  )
+  # This is pretty shitty but I haven't found a way
+  # to initiate a class without litterally calling it
+  $WebBrowser = Switch ($Name) {
+    'Chrome' { [Chrome]::new(); Break; }
+    Default { [Firefox]::new() }
+  }
+
+  ('ftp', 'http', 'https') | Foreach-Object {
+    Create-RegistryTree -Protocol $_ -WebBrowser $WebBrowser
+  }
+}
+
+# -----------------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------------
+DefaultBrowser -Name $BrowserName
